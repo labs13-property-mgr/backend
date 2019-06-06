dc1b4404054073ab0327da071f1975fb08781c02
ryanboris <ryan.boris@gmail.com>
2019-06-06 18:00:10 -0400

    fixes typo with serversetup typo

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const configureMiddleware = require('./middleware/serverSetup');
+const express = require('express')
+const configureMiddleware = require('./middleware/serversetup')
 
-const server = express();
-configureMiddleware(server);
+const server = express()
+configureMiddleware(server)
 
-server.get("/", async (req, res) => {
-  res.status(200).json({ message: "Server running...." });
-});
+server.get('/', async (req, res) => {
+  res.status(200).json({ message: 'Server running....' })
+})
 
 module.exports = server
