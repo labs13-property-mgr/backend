45c9992a199135496871b808c1113613695c1c96
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-06-06 14:03:07 -0400

    usertable and property table are created

--- data/migrations/20190606134844_create_properties_table.js
+++ data/migrations/20190606134844_create_properties_table.js
@@ -1,8 +1,12 @@
-
 exports.up = function(knex, Promise) {
+    return knex.schema.createTable("property", tbl => {
+      tbl.increments();
   
-};
-
-exports.down = function(knex, Promise) {
+      tbl.string("property_name", 128);
+      tbl.string("address", 100);
+    });
+  };
   
-};
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("property");
+  };
