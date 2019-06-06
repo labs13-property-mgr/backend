989d778f87a6e6f505def76a4739a76a776a1ce9
jkaddu <jkaddu@yahoo.com>
2019-06-06 08:48:52 -0700

    added knex dep, knexfile and gitignore

--- knexfile.js
+++ knexfile.js
@@ -1,38 +1,38 @@
-require("dotenv").config()
+require('dotenv').config()
 const dbConnection = process.env.DATABASE_URL
 
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      connection: {
-        filename: './data/db.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './data/migrations',
-      },
-      seeds: {
-        directory: './data/seeds',
-      },
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/db.db3'
     },
-    testing: {
-      client: 'sqlite3',
-      connection: {
-        filename: './data/test.db3',
-      },
-      useNullAsDefault: true,
-      migrations: {
-        directory: './data/migrations',
-      },
-      seeds: {
-        directory: './data/seeds',
-      },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
     },
-    production: {
-      client: "pg",
-      connection: dbConnection,
-      useNullAsDefault: true,
-      migrations: { directory: __dirname + '/data/migrations' }, 
-      seeds: { directory: __dirname + '/data/seeds' }
+    seeds: {
+      directory: './data/seeds'
     }
-  };
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    useNullAsDefault: true,
+    migrations: { directory: __dirname + '/data/migrations' },
+    seeds: { directory: __dirname + '/data/seeds' }
+  }
+}
