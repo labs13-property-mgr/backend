451fa4942dbf760c253c41ffc7484bf40250c952
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-06-11 11:11:34 -0400

    table config

--- data/migrations/20190610172944_create_service_order_table.js
+++ data/migrations/20190610172944_create_service_order_table.js
@@ -1,13 +1,16 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable("service_orders", tbl => {
       tbl.increments();
-      tbl.integer("tenant_id");
-      tbl.integer("owner_id");
       tbl.string("date_created", 100);
-      tbl.string("contractor", 100);
+      tbl.integer("property_id", 10).notnullable();//link to property
+      tbl.integer("tenant_id", 10).notnullable();//link to tenant
+      
+      tbl.string("notes", 1000);
+      tbl.string("contractor", 100);//link to contractor table?
       tbl.string("apointment", 100);
       tbl.string("followup", 100);
-      tbl.string("property_id", 100);
+      tbl.boolean("resolved_tenant");
+      tbl.boolean("resolved_owner");
     });
   };
   
