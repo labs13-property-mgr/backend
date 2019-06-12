fc839d6ce96fb0b55f6607c96db9509b93dcf9d7
LarrySimiyu <larry.simiyu@gmail.com>
2019-06-12 09:19:46 -0700

    tenants router and endpoints done

--- api/routes/tenant-router.js
+++ api/routes/tenant-router.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const router = require('express').Router()
 const Db = require('../models/tenant-model')
 
@@ -22,3 +23,86 @@ router.post('/', async (req, res) => {
 })
 
 module.exports = router
+=======
+const router = require("express").Router();
+// const db = require("../models/tenant-model");
+const db = require("../../data/dbConfig.js");
+
+// router.get("/", async (req, res) => {
+//   try {
+//     const tenant = await Db.find();
+//     res.status(200).json(tenant);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error.message);
+//   }
+// });
+
+// router.post("/", async (req, res) => {
+//   try {
+//     const tenant = await Db.add(req.body);
+//     res.status(201).json(tenant);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json(error.message);
+//   }
+// });
+
+// get list of tenants
+router.get("/", (req, res) => {
+  db("tenant")
+    .then(tenants => {
+      res.status(200).json(tenants);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+// post -  add tenants
+router.post("/", (req, res) => {
+  const tenant = req.body;
+
+  db("tenant")
+    .insert(tenant)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
+// put - edit tenant information
+router.put("/:id", (req, res) => {
+  db("tenant")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "tenant not found" });
+      }
+    });
+});
+
+// delete - delete individual tenants
+router.delete("/:id", (req, res) => {
+  db("tenant")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res.status(404).json({ message: "action not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
+>>>>>>> Stashed changes
---
+++ data/migrations/20190610135812_tenant.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("tenant", tenant => {
+    tenant.increments();
+
+    tenant.string("name", 100).notNullable();
+
+    tenant.string("Spouse Name", 100).notNullable();
+
+    tenant.string("additional adult name", 100);
+
+    tenant.integer("number in household", 100).notNullable();
+
+    tenant.string("child name", 100);
+
+    tenant.integer("contact info", 100).notNullable();
+
+    tenant.integer("emergency contact", 100).notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("tenant");
+};
+
+// Tenant Name, Spouse Name, Additional Adult Name, Number in Household, Child (first name),
+// Pets In Residence, type of pet, name of pet, Contact info for household, Emergency Contact Info
