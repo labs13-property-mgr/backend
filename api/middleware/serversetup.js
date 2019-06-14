e7a38e297b91796dd16f0dec9c994d37aae13955
ryanboris <ryan.boris@gmail.com>
2019-06-14 04:45:51 -0400

    fixes duplication in migrations

--- api/middleware/serversetup.js
+++ api/middleware/serversetup.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const userRouter = require("../routes/user-router");
-const propertyRouter = require("../routes/property-router");
-const tenantRouter = require("../routes/tenant-router");
-const serviceRouter = require("../routes/serviceRequest-router.js");
-
-module.exports = server => {
-  server.use(helmet());
-  server.use(express.json());
-  server.use(cors());
-  server.use("/api/users", userRouter);
-  server.use("/api/properties", propertyRouter);
-  server.use("/api/tenant", tenantRouter);
-  server.use("/api/service", serviceRouter);
-};
-=======
-<<<<<<< HEAD
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const userRouter = require('../routes/user-router');
-const propertyRouter = require('../routes/property-router');
-const tenantRouter = require('../routes/tenant-router');
-const serviceRouter = require('../routes/service-router');
-const vendorRouter = require('../routes/vendor-router');
-
-module.exports = (server) => {
-	server.use(helmet());
-	server.use(express.json());
-	server.use(cors());
-	server.use('/api/users', userRouter);
-	server.use('/api/properties', propertyRouter);
-	server.use('/api/tenant', tenantRouter);
-	server.use('/api/service', serviceRouter);
-	server.use('/api/vendor', vendorRouter);
-};
-=======
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
@@ -45,6 +5,7 @@ const userRouter = require('../routes/user-router')
 const propertyRouter = require('../routes/property-router')
 const tenantRouter = require('../routes/tenant-router')
 const serviceRouter = require('../routes/service-router')
+const vendorRouter = require('../routes/vendor-router')
 
 module.exports = server => {
   server.use(helmet())
@@ -54,6 +15,5 @@ module.exports = server => {
   server.use('/api/properties', propertyRouter)
   server.use('/api/tenant', tenantRouter)
   server.use('/api/service', serviceRouter)
+  server.use('/api/vendor', vendorRouter)
 }
->>>>>>> master
->>>>>>> master
