b1f9e1ee859ec1ff47d66a68337c019a5f3f0dc0
LarrySimiyu <larry.simiyu@gmail.com>
2019-06-17 12:42:04 -0700

    service request file route changed

--- api/middleware/serversetup.js
+++ api/middleware/serversetup.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const userRouter = require('../routes/user-router');
-const propertyRouter = require('../routes/property-router');
-const tenantRouter = require('../routes/tenant-router');
-const serviceRouter = require('../routes/service-router');
-const vendorRouter = require('../routes/vendor-router');
-const ownerRouter = require('../routes/owner-router');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const userRouter = require("../routes/user-router");
+const propertyRouter = require("../routes/property-router");
+const tenantRouter = require("../routes/tenant-router");
+const serviceRouter = require("../routes/serviceRequest-router");
+const vendorRouter = require("../routes/vendor-router");
+const ownerRouter = require("../routes/owner-router");
 
-module.exports = (server) => {
-	server.use(helmet());
-	server.use(express.json());
-	server.use(cors());
-	server.use('/api/users', userRouter);
-	server.use('/api/properties', propertyRouter);
-	server.use('/api/tenant', tenantRouter);
-	server.use('/api/service', serviceRouter);
-	server.use('/api/vendor', vendorRouter);
-	server.use('/api/owner', ownerRouter);
+module.exports = server => {
+  server.use(helmet());
+  server.use(express.json());
+  server.use(cors());
+  server.use("/api/users", userRouter);
+  server.use("/api/properties", propertyRouter);
+  server.use("/api/tenant", tenantRouter);
+  server.use("/api/service", serviceRouter);
+  server.use("/api/vendor", vendorRouter);
+  server.use("/api/owner", ownerRouter);
 };
