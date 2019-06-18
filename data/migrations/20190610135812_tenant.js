3e00185f68c6ea06a65597bb9d67c2e5519bc63d
ryanboris <ryan.boris@gmail.com>
2019-06-18 19:02:42 -0400

    deploy

--- data/migrations/20190610135812_tenant.js
+++ data/migrations/20190610135812_tenant.js
@@ -1,53 +1,27 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("tenant", tenant => {
+  return knex.schema.createTable('tenant', tenant => {
     tenant.increments();
-    tenant
-      .integer("property_id")
-      .unsigned()
-      .references("id")
-      .inTable("property")
-      .onUpdate("CASCADE");
-    tenant
-      .string("First_name", 50)
-      .references("First_name")
-      .inTable("users")
-      .notNullable()
-      .onUpdate("CASCADE");
-    tenant
-      .string("Last_name", 50)
-      .references("Last_name")
-      .inTable("users")
-      .notNullable()
-      .onUpdate("CASCADE");
-    tenant
-      .string("phone", 20)
-      .references("phone")
-      .inTable("users")
-      .notNullable()
-      .onUpdate("CASCADE");
-    tenant
-      .string("email", 50)
-      .notNullable()
-      .unique()
-      .references("email")
-      .inTable("users")
-      .onUpdate("CASCADE");
+    tenant.integer('property_id').unsigned();
+    tenant.string('firstName', 50);
+    tenant.string('lastName', 50);
+    tenant.string('phone', 20);
+    tenant.string('email', 50);
 
-    tenant.string("Spouse Name", 100);
+    tenant.string('Spouse Name', 100);
 
-    tenant.string("additional adult name", 100);
+    tenant.string('additional adult name', 100);
 
-    tenant.integer("number in household", 100);
+    tenant.integer('number in household', 100);
 
-    tenant.string("child name", 100);
+    tenant.string('child name', 100);
 
-    tenant.integer("emergency contact", 100);
-    tenant.boolean("active_tenant");
+    tenant.integer('emergency contact', 100);
+    tenant.boolean('active_tenant');
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("tenant");
+  return knex.schema.dropTableIfExists('tenant');
 };
 
 // Tenant Name, Spouse Name, Additional Adult Name, Number in Household, Child (first name),
