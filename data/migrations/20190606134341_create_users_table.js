008b1400ff630614735fce084a443319f9c1ec43
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-06-20 15:40:22 -0400

    Fixed users table to use the uid from firebase

--- data/migrations/20190606134341_create_users_table.js
+++ data/migrations/20190606134341_create_users_table.js
@@ -1,21 +1,22 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', tbl => {
+  return knex.schema.createTable("users", tbl => {
     tbl.increments();
-
     tbl
-      .string('username', 128)
+      .string("uid", 500)
       .notNullable()
       .unique();
-    tbl.string('password', 255).notNullable();
-    tbl.string('First_name', 50);
-    tbl.string('Last_name', 50);
-    tbl.string('phone', 20);
-    tbl.string('email', 50);
-    tbl.string('address', 100);
-    tbl.string('role', 50);
+    tbl.string("First_name", 50);
+    tbl.string("Last_name", 50);
+    tbl.string("phone", 20);
+    tbl
+      .string("email", 50)
+      .notNullable()
+      .unique();
+    tbl.string("address", 100);
+    tbl.string("role", 50);
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return knex.schema.dropTable("users");
 };
