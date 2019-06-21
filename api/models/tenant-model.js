afb71febab585287f2e0301a99e4b2492d01368a
LarrySimiyu <larry.simiyu@gmail.com>
2019-06-21 11:01:49 -0700

    added a router to retrieve tenant by id, be able to search a tenant by name

--- api/models/tenant-model.js
+++ api/models/tenant-model.js
@@ -3,19 +3,31 @@ const db = require("../../data/dbConfig.js");
 module.exports = {
   add,
   find,
+  get,
   findById
 };
 
 async function add(tenant) {
-  const [id] = await db("tenant as t").insert(tenant);
-  return findById(id)
-    .join("users as u", "t.user_id", "u.id")
-    .join("property as p", "t.property_id", "p.id")
-    .select("t.*", "u.name as tenant_name", "p.property_name");
+  const [id] = await db("tenant").insert(tenant);
+  //return findById(id)
+  return id;
+  //.join("users", "tenant.user_id", "users.id")
+  //.join("property", "tenant.property_id", "property.id");
+  //.select("tenant.*", "users.name as tenant_name", "property.property_name");
 }
 
 function find() {
   return db("tenant as t").join("users as s", "t.user_id", "s.id"); // this is how you join a table
+}
+
+function get(id) {
+  if (id) {
+    return db("tenant")
+      .where({ id: Number(id) })
+      .first();
+  } else {
+    return db("tenant");
+  }
 }
 
 function findById(id) {
--- api/routes/tenant-router.js
+++ api/routes/tenant-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Db = require("../models/tenant-model");
+const db = require("../models/tenant-model");
 
+// get list of tenants
 router.get("/", async (req, res) => {
   try {
-    const tenant = await Db.find();
+    const tenant = await db.find();
     res.status(200).json(tenant);
   } catch (error) {
     console.log(error);
@@ -12,15 +13,33 @@ router.get("/", async (req, res) => {
   }
 });
 
+//find tenant by their specified id ----l
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.get(id)
+    .then(tenant => {
+      if (tenant) {
+        res.status(200).json(tenant);
+      } else {
+        res.status(404).json({
+          message: "The child with the specifed ID does not exist"
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error.message);
+    });
+});
+
 // adds a tenant to a property ---
 router.post("/", async (req, res) => {
   const { property_id, user_id } = req.body; // have these in the body
 
-  if (!property_id || !user_id) {
+  if (!property_id) {
     res.status(422).json({ message: "I need a property and user id" });
   }
   try {
-    const tenant = await Db.add(req.body);
+    const tenant = await db.add(req.body);
     res.status(201).json(tenant);
   } catch (error) {
     console.log(error);
@@ -31,7 +50,7 @@ router.post("/", async (req, res) => {
 // update tenant info
 router.put("/:id", async (req, res) => {
   try {
-    const updated = await Db.update(req.params.id, req.body);
+    const updated = await db.update(req.params.id, req.body);
     if (updated) {
       res.status(200).json(updated);
     } else {
@@ -45,7 +64,7 @@ router.put("/:id", async (req, res) => {
 // delete a tenant
 router.delete("/:id", async (req, res) => {
   try {
-    const count = await Db.remove(req.params.id);
+    const count = await db.remove(req.params.id);
     if (count > 0) {
       res.status(200).json({ message: "Tenant deleted" });
     } else {
