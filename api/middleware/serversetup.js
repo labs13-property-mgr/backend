0694a78e965d368d9311721854812ecac5a3b4b2
ryanboris <ryan.boris@gmail.com>
2019-06-21 11:20:53 -0400

    removes duped routes

--- api/middleware/serversetup.js
+++ api/middleware/serversetup.js
@@ -7,13 +7,13 @@ const tenantRouter = require('../routes/tenant-router');
 const serviceRouter = require('../routes/service-router');
 const vendorRouter = require('../routes/vendor-router');
 
-module.exports = (server) => {
-	server.use(helmet());
-	server.use(express.json());
-	server.use(cors());
-	server.use('/api/user', userRouter);
-	server.use('/api/property', propertyRouter);
-	server.use('/api/tenant', tenantRouter);
-	server.use('/api/service', serviceRouter);
-	server.use('/api/vendor', vendorRouter);
+module.exports = server => {
+  server.use(helmet());
+  server.use(express.json());
+  server.use(cors());
+  server.use('/api/user', userRouter);
+  server.use('/api/property', propertyRouter);
+  server.use('/api/tenant', tenantRouter);
+  server.use('/api/service', serviceRouter);
+  server.use('/api/vendor', vendorRouter);
 };
--- api/routes/user-router.js
+++ api/routes/user-router.js
@@ -1,64 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models/user-model");
+const {add, find, findByEmail, remove, update, findPropByUser} = require("../models/user-model");
 
 //=====================================Generic Get all users
 router.get('/', async (req, res) => {
-	try {
-		const data = await Db.find('users');
-		res.status(200).json(data);
-	} catch (err) {
-		res.status(500).json({
-			error: err.message
-		});
-	}
-});
-
-// post a user
-router.post('/', async (req, res) => {
-	try {
-		const user = await Db.add(req.body);
-		res.status(201).json(user);
-	} catch (error) {
-		res.status(500).json(error.message);
-	}
+  try {
+    const data = await find('users');
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
 });
 
 router.get('/email', (req, res) => {
-	const { email } = req.body;
+  const { email } = req.body;
 
-	findByEmail(email)
-		.then((user) => {
-			res.status(200).json(user);
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+  findByEmail(email)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //--------------------get user by id
 router.get('/:id', (req, res) => {
-	const user_id = req.params.id;
-	Db.findById(user_id)
-		.then((user) => {
-			if (user) {
-				res.status(200).json(user);
-			} else {
-				res.status(404).json({ message: 'user not found' });
-			}
-		})
-		.catch((error) => {
-			if (error) {
-				res.status(500).json({ message: `Error : ${error}` });
-			}
-		});
+  const user_id = req.params.id;
+  findById(user_id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: 'user not found' });
+      }
+    })
+    .catch((error) => {
+      if (error) {
+        res.status(500).json({ message: `Error : ${error}` });
+      }
+    });
 });
 
 //=====================================User Property routes
 //--------------------get properties by user id
 router.get("/:id/properties", async (req, res) => {
   const user_id = req.params.id;
-  db.findPropByUser(user_id)
+  findPropByUser(user_id)
     .then(properties => {
       if (properties) {
         res.status(200).json(properties);
@@ -74,59 +64,44 @@ router.get("/:id/properties", async (req, res) => {
         .status(500)
         .json({ message: `The properties seems to be lost try again` });
     });
-router.get('/:id/properties', async (req, res) => {
-	const user_id = req.params.id;
-	Db.findPropByUser(user_id)
-		.then((properties) => {
-			if (properties) {
-				res.status(200).json(properties);
-			} else {
-				res.status(404).json({
-					Message: 'These properties are lost like the Donner party...sad indeed'
-				});
-			}
-		})
-		.catch((err) => {
-			res.status(500).json({ message: `The properties seems to be lost try again` });
-		});
-});
+
 
 // update a user
 router.put('/:id', async (req, res) => {
-	try {
-		const updated = await Db.update(req.params.id, req.body);
-		if (updated) {
-			res.status(200).json(updated);
-		} else {
-			res.status(404).json({ message: 'User ID not found' });
-		}
-	} catch (error) {
-		res.status(500).json(error.message);
-	}
+  try {
+    const updated = await update(req.params.id, req.body);
+    if (updated) {
+      res.status(200).json(updated);
+    } else {
+      res.status(404).json({ message: 'User ID not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 // delete a user
 router.delete('/:id', async (req, res) => {
-	try {
-		const count = await Db.remove(req.params.id);
-		if (count > 0) {
-			res.status(200).json({ message: 'User Deleted' });
-		} else {
-			res.status(404).json({ message: 'User ID not found' });
-		}
-	} catch (error) {
-		res.status(500).json(error.message);
-	}
+  try {
+    const count = await remove(req.params.id);
+    if (count > 0) {
+      res.status(200).json({ message: 'User Deleted' });
+    } else {
+      res.status(404).json({ message: 'User ID not found' });
+    }
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
 
 //=======================================Create new user
 router.post("/", async (req, res) => {
   try {
     const userData = req.body;
-    const checkEmail = await db.findByEmail(userData.email);
+    const checkEmail = await findByEmail(userData.email);
     if (!checkEmail) {
       try {
-        const userId = await db.add(userData);
+        const userId = await add(userData);
         res.status(201).json(userId);
       } catch (error) {
         res.status(500).json({ error: "Unable to add user to database" });
