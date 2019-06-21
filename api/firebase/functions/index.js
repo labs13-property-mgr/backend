95a3fa9a4a6bc804afd24054ea922e98b00ff7a7
BrandonSaravia <BPSaravia@gmail.com>
2019-06-21 10:57:43 -0400

    able to get urls from storage on google/storage

--- api/firebase/functions/index.js
+++ api/firebase/functions/index.js
@@ -1,3 +1,11 @@
+const functions = require('firebase-functions');
+const os = require('os');
+const path = require('path');
+const spawn = require('child-process-promise').spawn;
+const cors = require('cors')({ origin: true });
+const Busboy = require('busboy');
+const fs = require('fs');
+
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./config");
@@ -6,20 +14,6 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://rentme-52af4.firebaseio.com"
 });
-
-
-const functions = require('firebase-functions');
-
-const os = require('os');
-const path = require('path');
-const spawn = require('child-process-promise').spawn;
-// const express = require('express');
-// const router = express.Router();
-const cors = require('cors')({ origin: true });
-// const functions = require('firebase-functions');
-const Busboy = require('busboy');
-const fs = require('fs');
-// const { find } = require('../models/user-model');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -36,10 +30,12 @@ const storage = new Storage({
 
 exports.onFileChange = functions.storage.object().onFinalize(event => {
     const bucket = event.bucket;
-    console.log(event);
+    
     const contentType = event.contentType;
     const filePath = event.name;
     console.log('File change detected, function execution started');
+
+
 
     if (event.id === null) {
         console.log('We deleted a file, exit...');
@@ -48,6 +44,7 @@ exports.onFileChange = functions.storage.object().onFinalize(event => {
 
     if (path.basename(filePath).startsWith('resized-')) {
         console.log('We already resized that file!');
+        currentFileName = filePath; 
         return;
     }
 
@@ -60,20 +57,22 @@ exports.onFileChange = functions.storage.object().onFinalize(event => {
     return destBucket.file(filePath).download({
         destination: tmpFilePath
     }).then(() => {
-
+        
         return spawn('convert', [tmpFilePath, '-resize', '100x100', tmpFilePath]);
 
     }).then(() => {
-
+        console.log(event);
         return destBucket.upload(tmpFilePath, {
 
             destination: 'resized-' + path.basename(filePath),
             metadata: metadata
 
         })
+    }).then(() => {
+        return fs.unlinkSync(tempFilePath);
     })
-
 });
+
 
 
 exports.uploadFile = functions.https.onRequest((req, res) => {
@@ -87,12 +86,13 @@ exports.uploadFile = functions.https.onRequest((req, res) => {
 
         let uploadData = null;
 
+    
         busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
             const filePath = path.join(os.tmpdir(), filename);
             uploadData = { file: filePath, type: mimetype };
             file.pipe(fs.createWriteStream(filePath));
         });
-        // busboy.on('field');
+
         busboy.on('finish', () => {
             const bucket = storage.bucket('rentme-52af4.appspot.com');
             bucket.upload(uploadData.file, {
@@ -102,9 +102,12 @@ exports.uploadFile = functions.https.onRequest((req, res) => {
                         contentType: uploadData.type
                     }
                 }
+
+
             }).then(() => {
+
                 return res.status(200).json({
-                    message: 'It worked!'
+                    message: 'it worked'
                 });
             })
             .catch(err => {
@@ -119,35 +122,44 @@ exports.uploadFile = functions.https.onRequest((req, res) => {
 });
 
 
-// module.uploadFile = functions.https.onRequest((req, res) => {
-//     cors((req, res) => {
-//         const busboy = new busboy({ headers: req.headers });
-//         let uploadDate = mull;
 
-//         busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-//             const filepath = path.join(os.tmpdir(), filename);
-//             uploadDate = { file: filepath, type: mimetype };
-//             file.pipe(fs.createWriteStream(filepath));
-//         });
+exports.getFile = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        if (req.method !== 'GET') {
+            return res.status(500).json({
+                message: 'Not Allowed'
+            })
+        }
+    
 
-//         busboy.on('finish', () => {
-//             const bucket = gcs.bucket();
-//             bucket
-//                 .upload(uploadData.file, {
-//                     uploadType: 'media',
-//                     metadata: {
-//                         metadata: {
-//                             contentType: uploadData.type
-//                         }
-//                     }
-//                 })
-//                 .then(() => {
-//                     res.send(200).json({ message: 'It works!' });
-//                 })
-//                 .catch((err) => {
-//                     res.status(500).json({ error: err });
-//                 });
-//         });
-//         busboy.end(req.rawBody);
-//     });
-// });
+        const bucketName = 'rentme-52af4.appspot.com';
+
+        
+        
+
+
+        storage
+            .bucket(bucketName)
+            .getFiles()
+            .then(results => {
+                const files = results[0];
+
+                console.log('Files:');
+                return files.forEach(file => {
+                    
+                    // console.log(file, file.metadata);
+                    return file.getSignedUrl({
+                        action: 'read',
+                        expires: '03-09-2491'
+                    }).then(signedUrls => {
+
+                        console.log(signedUrls)
+                        return;
+                    });
+                });
+            })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+    });
+});
