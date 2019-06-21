d410eb198e6968f3f009b8045effa20c29366c6d
ryanboris <ryan.boris@gmail.com>
2019-06-21 11:38:34 -0400

    fixes duped property in tenant migration

--- data/migrations/20190610135812_tenant.js
+++ data/migrations/20190610135812_tenant.js
@@ -1,30 +1,32 @@
 exports.up = function(knex, Promise) {
-	return knex.schema.createTable('tenant', (tenant) => {
-		tenant.increments(); // Tenant PRimary Key
+  return knex.schema.createTable('tenant', tenant => {
+    tenant.increments(); // Tenant PRimary Key
 
-		tenant.string('property_id');
-		tenant.string('First_name', 50);
-		tenant.string('Last_name', 50);
-		tenant.string('phone', 20).notNullable();
-		tenant.string('email', 50).notNullable().unique();
-		tenant.string('Spouse Name', 100);
-		tenant.string('additional adult name', 100);
-		tenant.integer('number in household', 100);
-		tenant.string('child name', 100);
-		tenant.integer('emergency contact', 100);
-		tenant.boolean('active_tenant');
-		tenant
-			.string('property_id')
-			.notNullable()
-			.references('id')
-			.inTable('property')
-			.onDelete('CASCADE')
-			.onUpdate('CASCADE');
-	});
+    tenant.string('First_name', 50);
+    tenant.string('Last_name', 50);
+    tenant.string('phone', 20).notNullable();
+    tenant
+      .string('email', 50)
+      .notNullable()
+      .unique();
+    tenant.string('Spouse Name', 100);
+    tenant.string('additional adult name', 100);
+    tenant.integer('number in household', 100);
+    tenant.string('child name', 100);
+    tenant.integer('emergency contact', 100);
+    tenant.boolean('active_tenant');
+    tenant
+      .string('property_id')
+      .notNullable()
+      .references('id')
+      .inTable('property')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  });
 };
 
 exports.down = function(knex, Promise) {
-	return knex.schema.dropTableIfExists('tenant');
+  return knex.schema.dropTableIfExists('tenant');
 };
 
 // Tenant Name, Spouse Name, Additional Adult Name, Number in Household, Child (first name),
