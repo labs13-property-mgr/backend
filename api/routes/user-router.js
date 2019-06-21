b4e2be80261792e324717eb3959fcf6d24c60da6
ryanboris <ryan.boris@gmail.com>
2019-06-20 23:41:48 -0400

    fixes merge conflict dupe

--- api/routes/user-router.js
+++ api/routes/user-router.js
@@ -3,48 +3,6 @@ const router = express.Router();
 const db = require("../models/user-model");
 
 //=====================================Generic Get all users
-router.get("/", async (req, res) => {
-  const data = await db.find("users");
-  try {
-    res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({
-      error: err.message
-    });
-  }
-});
-
-router.get("/email", (req, res) => {
-  const { email } = req.body;
-
-  db.findByEmail(email)
-    .then(user => {
-      res.status(200).json(user);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
-
-//--------------------get user by id
-router.get("/:id", (req, res) => {
-  const user_id = req.params.id;
-  db.findById(user_id)
-    .then(user => {
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({ message: "user not found" });
-      }
-    })
-    .catch(error => {
-      if (error) {
-        res.status(500).json({ message: `Error : ${error}` });
-      }
-    });
-const Db = require('../models/user-model.js');
-
-//=====================================Generic Get all users
 router.get('/', async (req, res) => {
 	try {
 		const data = await Db.find('users');
