cd9892537f0aec5a86e2a4a6880dd40aa928f938
ryanboris <ryan.boris@gmail.com>
2019-06-25 03:46:58 -0400

    adds deploy

--- api/models/tenant-model.js
+++ api/models/tenant-model.js
@@ -12,9 +12,6 @@ async function add(tenant) {
   const [id] = await db('tenant').insert(tenant);
   //return findById(id)
   return id;
-  //.join("users", "tenant.user_id", "users.id")
-  //.join("property", "tenant.property_id", "property.id");
-  //.select("tenant.*", "users.name as tenant_name", "property.property_name");
 }
 
 //larry simiyu test addition
