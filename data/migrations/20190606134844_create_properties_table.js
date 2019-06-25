bf21d675d5788dbf60a8b107b3b30bde6ad65577
ryanboris <ryan.boris@gmail.com>
2019-06-25 03:10:56 -0400

    deploy

--- data/migrations/20190606134844_create_properties_table.js
+++ data/migrations/20190606134844_create_properties_table.js
@@ -1,7 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('property', tbl => {
-    tbl.increments(); // Property Primary Key
-
+    tbl.increments();
     tbl.string('property_name', 128);
     tbl.string('address', 100);
     tbl
--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-const dbConnection = process.env.DATABASE_URL
-console.log(dbConnection)
+require('dotenv').config();
+const dbConnection = process.env.DATABASE_URL;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -15,24 +15,11 @@ module.exports = {
       directory: './data/seeds'
     }
   },
-  testing: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/test.db3'
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations'
-    },
-    seeds: {
-      directory: './data/seeds'
-    }
-  },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: dbConnection,
     useNullAsDefault: true,
     migrations: { directory: __dirname + '/data/migrations' },
     seeds: { directory: __dirname + '/data/seeds' }
   }
-}
+};
