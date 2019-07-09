e706efe9079399c91b4bcdfadef82296c6f77a03
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-07-09 12:29:54 -0400

    fixed

--- data/migrations/20190708170952_create_receipts_table.js
+++ data/migrations/20190708170952_create_receipts_table.js
@@ -15,5 +15,5 @@ exports.up = function(knex, Promise) {
   };
   
   exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('property_image');
+    return knex.schema.dropTableIfExists('receipt');
   };
