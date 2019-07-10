2755f692bbcc70d1d2d562892cfa467f7db316c1
Josh Armantrout <44445530+josharmantrout91@users.noreply.github.com>
2019-07-10 18:35:43 -0400

    Revert "Service order property"

--- api/models/user-model.js
+++ api/models/user-model.js
@@ -1,4 +1,4 @@
-const db = require("../../data/dbConfig.js");
+const db = require('../../data/dbConfig.js');
 
 module.exports = {
   find,
@@ -10,51 +10,38 @@ module.exports = {
   add
 };
 
-async function add(user) {
-  const [id] = await db("users").insert(user);
-  return findById(id);
-
+function add(user) {
+  return db('users').insert(user);
 }
 
 function find() {
-  return db("users");
+  return db('users');
 }
 
 function findById(id) {
-  return db("users")
+  return db('users')
     .where({ id })
     .first();
 }
 
 function findByEmail(email) {
-  return db("users")
+  return db('users')
     .where({ email })
     .first();
 }
 
 async function findPropByUser(owner_id) {
-  return await db("property").where({ owner_id });
+  return await db('property').where({ owner_id });
 }
 
 function update(id, changes) {
-  return db("users")
+  return db('users')
     .where({ id })
     .update(changes);
 }
 
 function remove(id) {
-  return db("users")
+  return db('users')
     .where({ id })
     .del();
 }
-
-
-//larry simiyu test code
-function insert(user) {
-  return db("users")
-    .insert(user)
-    .then(ids => ({
-      id: ids[0]
-    }));
-}
-
