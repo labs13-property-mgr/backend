b7ccb2477a96b22edd1e39e8b39b512fd6e2a068
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-07-11 11:50:52 -0400

    added length to the string

--- data/migrations/20190606134844_create_properties_table.js
+++ data/migrations/20190606134844_create_properties_table.js
@@ -1,17 +1,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("property", tbl => {
     tbl.increments();
-    tbl.string("property_name").notNullable();
-    tbl.string("address").notNullable();
-    tbl.string("unit");
-    tbl.string("city").notNullable();
-    tbl.string("state").notNullable();
-    tbl.string("zip").notNullable();
-    tbl.string("rent");
-    tbl.string("image_url");
+    tbl.string("property_name", 150).notNullable();
+    tbl.string("address", 150).notNullable();
+    tbl.string("unit", 150);
+    tbl.string("city", 150).notNullable();
+    tbl.string("state", 150).notNullable();
+    tbl.string("zip", 150).notNullable();
+    tbl.string("rent", 150);
+    tbl.string("image_url", 250);
     tbl
 
-      .string("owner_id")
+      .string("owner_id", 150)
       .references("uid")
       .inTable("users")
       .onDelete("CASCADE")
